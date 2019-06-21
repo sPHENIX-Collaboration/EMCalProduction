3e3e44ae44f89919b3475dd1d0050cee1abd016c
Cher-Fading <xw31@illinois.edu>
2019-06-21 16:57:58 -0500

    removed the permission of students account to modify google drive directly from NPL computers

--- analyze_number.sh
+++ analyze_number.sh
@@ -1,32 +1,37 @@
-for folder in 20190603
-do
-cd ~/sPHENIX/LightTransmission
-source ~/anaconda3/etc/profile.d/conda.sh
-eval "$(conda shell.bash hook)"
-conda activate ocr
+for folder in 20190617
+  do
+  cd ~/sPHENIX/LightTransmission
+  source ~/anaconda3/etc/profile.d/conda.sh
+  eval "$(conda shell.bash hook)"
+  conda activate ocr
 
-mkdir "pictures/$folder/Analysis"
-mkdir "pictures/$folder/Original"
-mkdir "pictures/$folder/Temp"
-	for entry in pictures/$folder/*.JPG
-	do
-  		echo ${entry:(-12):12}
-  		root -b -q -l 'crop.cpp("'${entry:(-12):8}'","'${entry:9:(${#entry}-13)}'","'pictures/cropped'","'pictures/$folder/Temp'")'
-  		convert pictures/$folder/Temp/${entry:(-12):8}-number.JPG -threshold 10% pictures/$folder/Temp/${entry:(-12):8}-numberbw.JPG 
-  		dbnpic=$(python number.py pictures/$folder/Temp/${entry:(-12):8}-numberbw.JPG pictures/templates 2>&1)
-  		mv $entry pictures/$folder/Original/$dbnpic
-  		mv pictures/cropped/${entry:(-12):8}-block.JPG pictures/cropped/$dbnpic
-  		root -b -q -l 'fiberCounter_number.C("'${dbnpic:(-10):4}'","'${dbnpic:(-5):1}'","'pictures/cropped'","'pictures/$folder/Analysis'","'pictures/$folder/Analysis/$folder\_2ends.csv'")'
-  		rm pictures/$folder/Temp/${entry:(-12):8}-number.JPG
-  		rm pictures/$folder/Temp/${entry:(-12):8}-numberbw.JPG
-	done
-rm -r pictures/$folder/Temp
-root -l -q -b 'getBetter2.cpp("'pictures/$folder/Analysis'", "'$folder\_2ends.csv'","'pictures/$folder/Analysis/$folder\_1end.csv'","'$folder'")'
-cp 'analyze_number.sh' "pictures/$folder/Analysis"
+  mkdir "pictures/$folder/Analysis"
+  mkdir "pictures/$folder/Original"
+  mkdir "pictures/$folder/Temp"
+  for entry in pictures/$folder/*.JPG
+	 do
+    echo ${entry:(-12):12}
+    root -b -q -l 'crop.cpp("'${entry:(-12):8}'","'${entry:0:(${#entry}-13)}'","'pictures/cropped'","'pictures/$folder/Temp'")'
+    convert pictures/$folder/Temp/${entry:(-12):8}-number.JPG -threshold 10% pictures/$folder/Temp/${entry:(-12):8}-numberbw.JPG 
+  	dbnpic=$(python number.py pictures/$folder/Temp/${entry:(-12):8}-numberbw.JPG pictures/templates 2>&1)
+  	mv $entry pictures/$folder/Original/$dbnpic
+  	mv pictures/cropped/${entry:(-12):8}-block.JPG pictures/cropped/$dbnpic
+  	root -b -q -l 'fiberCounter_number.C("'${dbnpic:(-10):4}'","'${dbnpic:(-5):1}'","'pictures/cropped'","'pictures/$folder/Analysis'","'pictures/$folder/Analysis/$folder\_2ends.csv'")'
+  	rm pictures/$folder/Temp/${entry:(-12):8}-number.JPG
+  	rm pictures/$folder/Temp/${entry:(-12):8}-numberbw.JPG
+    #cp pictures/cropped/$dbnpic ~/google-drive/Light\ Transmission\ Test/Block\ pictures/Cropped
+  done
+  rm -r pictures/$folder/Temp
+  root -l -q -b 'getBetter2.cpp("'pictures/$folder/Analysis'", "'$folder\_2ends.csv'","'pictures/$folder/Analysis/$folder\_1end.csv'","'$folder'")'
+  cp 'analyze_number.sh' "pictures/$folder/Analysis"
 
-cp pictures/$folder/Analysis/$folder\_1end.csv  updatedb/blocks.csv
-cd updatedb
-conda deactivate
-conda deactivate
-python update_fibercounting.py
+  cp pictures/$folder/Analysis/$folder\_1end.csv  updatedb/blocks.csv
+  #cp -r pictures/$folder/Original ~/google-drive/Light\ Transmission\ Test/Block\ pictures/Original/$folder
+  #cp -r pictures/$folder/Analysis ~/google-drive/Light\ Transmission\ Test/Analysis/$folder\_pic$folder
+  cd updatedb
+  conda deactivate
+  conda deactivate
+  python update_fibercounting.py
+
+
 done
