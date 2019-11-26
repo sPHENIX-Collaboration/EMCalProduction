924f28dceac737f2cc6b84286a8c984102a695e6
Cher Wang <cherwang@Chers-MacBook-Pro-2.local>
2019-11-26 18:50:39 -0400

    usable analyze.sh

--- analyze.sh
+++ analyze.sh
@@ -1,21 +1,27 @@
+source /home/sickles-lab/anaconda3/etc/profile.d/conda.sh
+eval "$(conda shell.bash hook)"
+
 for folder in 20190514
 do
 . '/usr/local/build/bin/thisroot.sh'	
-cd ~/sPHENIX/LightTransmission
+cd /home/sickles-lab/sPHENIX/LightTransmission
 mkdir "pictures/$folder/Analysis"
 	for dbn in 393 346 683 183
 	do
     	for end in N W
     	do
 		echo processing DBN_$dbn-$end
-		root -l -q -b 'fiberCounter.C('$dbn',"'$end'","'pictures/$folder/Original'","'pictures/cropped'","'pictures/$folder/Analysis'","'pictures/$folder/Analysis/$folder\_2ends.csv'")'
+		root -l -q -b 'fiberCounter_number.C('$dbn',"'$end'","'pictures/cropped'","'pictures/$folder/Analysis'","'pictures/$folder/Analysis/$folder\_2ends.csv'")'
     	done
 	done
 root -l -q -b 'getBetter2.cpp("'pictures/$folder/Analysis'", "'$folder\_2ends.csv'","'pictures/$folder/Analysis/$folder\_1end.csv'","'$folder'")'
 cp 'analyze.sh' "pictures/$folder/Analysis"
 
 cp pictures/$folder/Analysis/$folder\_1end.csv  updatedb/blocks.csv
+cp -r pictures/$folder/Original ~/google-drive/Light\ Transmission\ Test/Block\ pictures/Original/$folder
+cp -r pictures/$folder/Analysis ~/google-drive/Light\ Transmission\ Test/Analysis/$folder\_pic$folder
 cd updatedb
+conda deactivate
+conda deactivate
 python update_fibercounting.py
 done
-
