2d0c6f4074cd14622a7dac00f8981d13e906f40e
Cher_Fading <xw31@illinois.edu>
2019-11-28 15:06:21 -0600

    analyze.sh can be used to process manually cropped and named photos

--- analyze.sh
+++ analyze.sh
@@ -1,27 +1,27 @@
-source /home/sickles-lab/anaconda3/etc/profile.d/conda.sh
-eval "$(conda shell.bash hook)"
+#source /home/sickles-lab/anaconda3/etc/profile.d/conda.sh
+#eval "$(conda shell.bash hook)"
 
-for folder in 20190514
+for folder in '20191126-1'
 do
-. '/usr/local/build/bin/thisroot.sh'	
-cd /home/sickles-lab/sPHENIX/LightTransmission
-mkdir "pictures/$folder/Analysis"
-	for dbn in 393 346 683 183
+#. '/usr/local/build/bin/thisroot.sh'	
+#cd /home/sickles-lab/sPHENIX/LightTransmission
+mkdir "$folder/Analysis"
+	for dbn in 275
 	do
     	for end in N W
     	do
 		echo processing DBN_$dbn-$end
-		root -l -q -b 'fiberCounter_number.C('$dbn',"'$end'","'pictures/cropped'","'pictures/$folder/Analysis'","'pictures/$folder/Analysis/$folder\_2ends.csv'")'
+		root -l -q -b 'fiberCounter_number.C("'$dbn'","'$end'","'cropped'","'$folder/Analysis'","'$folder/Analysis/$folder\_2ends.csv'")'
     	done
 	done
-root -l -q -b 'getBetter2.cpp("'pictures/$folder/Analysis'", "'$folder\_2ends.csv'","'pictures/$folder/Analysis/$folder\_1end.csv'","'$folder'")'
-cp 'analyze.sh' "pictures/$folder/Analysis"
+root -l -q -b 'getBetter2.cpp("'$folder/Analysis'", "'$folder\_2ends.csv'","'$folder/Analysis/$folder\_1end.csv'","'$folder'")'
+cp 'analyze.sh' "$folder/Analysis"
 
-cp pictures/$folder/Analysis/$folder\_1end.csv  updatedb/blocks.csv
-cp -r pictures/$folder/Original ~/google-drive/Light\ Transmission\ Test/Block\ pictures/Original/$folder
-cp -r pictures/$folder/Analysis ~/google-drive/Light\ Transmission\ Test/Analysis/$folder\_pic$folder
-cd updatedb
-conda deactivate
-conda deactivate
-python update_fibercounting.py
+#cp pictures/$folder/Analysis/$folder\_1end.csv  updatedb/blocks.csv
+#cp -r pictures/$folder/Original ~/google-drive/Light\ Transmission\ Test/Block\ pictures/Original/$folder
+#cp -r pictures/$folder/Analysis ~/google-drive/Light\ Transmission\ Test/Analysis/$folder\_pic$folder
+#cd updatedb
+#conda deactivate
+#conda deactivate
+#python update_fibercounting.py
 done
