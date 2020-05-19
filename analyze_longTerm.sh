93320414cb6243002bc461471a9dc96a979712cc
Xiaoning-Wang <xw31@illinois.edu>
2020-05-19 15:16:07 -0500

    Update analyze_longTerm.sh

--- analyze_longTerm.sh
+++ analyze_longTerm.sh
@@ -7,24 +7,26 @@ for folder in $1
   eval "$(conda shell.bash hook)"
   conda activate ocr
 
-  mkdir "pictures/monitoring/$folder/Analysis"
-  mkdir "pictures/monitoring/$folder/Original"
-  mkdir "pictures/monitoring/$folder/Temp"
+  mkdir "pictures/$folder/Analysis"
+  mkdir "pictures/$folder/Original"
+  mkdir "pictures/$folder/Temp"
   for entry in pictures/$folder/*.JPG
-	 do
-    echo ${entry:(-12):12}
-    root -b -q -l 'crop.cpp("'${entry:(-12):8}'","'${entry:0:(${#entry}-13)}'","'pictures/monitoring/cropped'","'pictures/monitoring/$folder/Temp'")'
-    convert pictures/monitoring/$folder/Temp/${entry:(-12):8}-number.JPG -threshold 10% pictures/monitoring/$folder/Temp/${entry:(-12):8}-numberbw.JPG 
-  	dbnpic=$(python number.py pictures/monitoring/$folder/Temp/${entry:(-12):8}-numberbw.JPG pictures/templates 2>&1)
-  	mv $entry pictures/monitoring/$folder/Original/$dbnpic
-  	mv pictures/monitoring/cropped/${entry:(-12):8}-block.JPG pictures/monitoring/cropped/$dbnpic
-  	root -b -q -l 'fiberCounter_number.C("'${dbnpic:(-10):4}'","'${dbnpic:(-5):1}'","'pictures/monitoring/cropped'","'pictures/monitoring/$folder/Analysis'","'pictures/monitoring/$folder/Analysis/$folder\_2ends.csv'")'
-  	rm pictures/monitoring/$folder/Temp/${entry:(-12):8}-number.JPG
-  	rm pictures/monitoring/$folder/Temp/${entry:(-12):8}-numberbw.JPG
-    cp pictures/monitoring/cropped/$dbnpic ~/google-drive/Light\ Transmission\ Test/Block\ pictures/Monitoring/Cropped
+  do
+    	tmpf=${entry%/*}
+    	tmp=${entry#*/}
+    	b=${tmp%.*}
+	echo $b
+    	root -b -q -l 'crop.cpp("'$b'","'$tmpf'","'pictures/cropped'","'pictures/$folder/Temp'")'
+    	convert pictures/$folder/Temp/$b-number.JPG -threshold 10% pictures/$folder/Temp/$b-numberbw.JPG 
+  	dbnpic=$(python number.py pictures/$folder/Temp/$b-numberbw.JPG pictures/templates 2>&1)
+  	mv $entry pictures/$folder/Original/$dbnpic
+  	mv pictures/monitoring/cropped/$b-block.JPG pictures/cropped/$dbnpic
+  	root -b -q -l 'fiberCounter_number.C("'${dbnpic:(-10):4}'","'${dbnpic:(-5):1}'","'pictures/cropped'","'pictures/$folder/Analysis'","'pictures/$folder/Analysis/$folder\_2ends.csv'")'
+  	rm pictures/$folder/Temp/$b-number.JPG
+  	rm pictures/$folder/Temp/$b-numberbw.JPG
   done
   rm -r pictures/monitoring/$folder/Temp
-  root -l -q -b 'getBetter2.cpp("'pictures/monitoring/$folder/Analysis'", "'$folder\_2ends.csv'","'pictures/monitoring/$folder/Analysis/$folder\_1end.csv'","'$folder'")'
+  root -l -q -b 'getBetter2.cpp("'pictures/$folder/Analysis'", "'$folder\_2ends.csv'","'pictures/$folder/Analysis/$folder\_1end.csv'","'$folder'")'
   #cp 'analyze_number.sh' "pictures/$folder/Analysis"
 
   #cp pictures/monitoring/$folder/Analysis/$folder\_1end.csv  updatedb_monitor/blocks.csv
